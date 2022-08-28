7c9c91667ccb9e105c5ef88bee8e997302add45b
konrad <king.koxy@gmail.com>
2022-08-28 15:54:21 +0200

    initial commit

---
+++ ophase/src/lib/app/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<div class="fixed w-full">
+    <div class="bg-grasshopperGreen-700">
+        <img src="" alt="https://picsum.photos/200/300">
+    </div>
+</div>
